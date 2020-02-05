b9be61baad83b5f3cc9ddcb4a128866e7f68c9eb
Alexander Reisach <alexander.reisach@gmail.com>
2020-02-05 20:22:27 +0100

    first try at pso

--- plots.py
+++ plots.py
@@ -1,42 +1,46 @@
-from mpl_toolkits import mplot3d
-
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 from matplotlib import cm
 
 
-### Rosenbrock function
+### Rosenbrock
+def plt_rosenbrock():
+    fig = plt.figure()
+    ax = fig.gca(projection="3d")
+    X = np.arange(-2, 2, 0.1)
+    Y = np.arange(-2, 2, 0.1)
+    X, Y = np.meshgrid(X, Y)
+    a = 0
+    b = 1000
+    Z = np.add(
+        np.square(np.subtract(a, X)),
+        np.multiply(b, np.square(np.subtract(Y, np.square(X))))
+    )
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.plasma, linewidth=0,
+    antialiased=False)
+    plt.show()
 
-fig = plt.figure()
-ax = fig.gca(projection="3d")
-X = np.arange(-2, 2, 0.1)
-Y = np.arange(-2, 2, 0.1)
-X, Y = np.meshgrid(X, Y)
-a = 0
-b = 1000
-Z = np.add(
-    np.square(np.subtract(a, X)),
-    np.multiply(b, np.square(np.subtract(Y, np.square(X))))
-)
-surf = ax.plot_surface(X, Y, Z, cmap=cm.plasma, linewidth=0,
- antialiased=False)
-plt.show()
-
-
-### Rastrigin function
 
 def rastrigin(*X, **kwargs):
     A = kwargs.get('A', 10)
     return A*len(X) + sum([(x**2 - A * np.cos(2 * np.pi * x)) for x in X])
 
-fig = plt.figure()
-ax = fig.gca(projection="3d")
-X = np.arange(-2, 2, 0.1)
-Y = np.arange(-2, 2, 0.1)
-X, Y = np.meshgrid(X, Y)
-Z = rastrigin(X, Y, A=1)
-surf = ax.plot_surface(X, Y, Z, cmap=cm.plasma, linewidth=0,
- antialiased=False)
-plt.show()
+### Rastrigin
+def plt_rastrigin():
+    fig = plt.figure()
+    ax = fig.gca(projection="3d")
+    X = np.arange(-2, 2, 0.1)
+    Y = np.arange(-2, 2, 0.1)
+    X, Y = np.meshgrid(X, Y)
+    Z = rastrigin(X, Y, A=1)
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.plasma, linewidth=0,
+    antialiased=False)
+    plt.show()
+
+
+if __name__ == "__main__":
+    plt_rosenbrock()
+    plt_rastrigin()    
 
 
--- pso.py
+++ pso.py
@@ -2,16 +2,55 @@
 initialize swarm, update location on each iteration
 """
 import numpy as np
+from copy import deepcopy
+from plots import rastrigin
+
+best_fitness = np.inf
+best_position = np.array([0, 0])
 
 def initialize(n, x_min, x_max, y_min, y_max):
-    initial_positions = [
-        (np.random.uniform(x_min, x_max), np.random.uniform(y_min, y_max))
+    particles = [
+        {
+            "pos": np.array([np.random.uniform(x_min, x_max),
+                             np.random.uniform(y_min, y_max)]),
+            "v": np.array([np.random.uniform(0, 0.1),
+                           np.random.uniform(0, 0.1)]),
+            "fit": np.inf
+        }
         for i in range(n)
     ]
-    return initial_positions
+    for p in particles:
+        p["best_pos"] = (p["pos"])
+    return particles
 
-def update(positions):
-    pass
+def update(particles, fitness_func):
+    global best_fitness
+    global best_position
+    update = []
+    for p in particles:
+        r = np.random.uniform(0, 1)
+        v = (
+            p["v"] +
+            r * (p["pos"] - p["best_pos"]) +
+            r * (p["pos"] - best_position)
+        )
+        update.append(v)
+    for i, p in enumerate(particles):
+        p["pos"] += update[i]
+        p["fit"] = fitness_func(p["pos"])
+        if p["fit"] > best_fitness:
+            best_fitness = p["fit"]
+            best_position = p["pos"]
+    return particles
+
+def train(num_particles, num_iter, fitness_func):
+    particles = initialize(num_particles, -2, 2, -2, 2)
+    history = []
+    for i in range(num_iter):
+        particles = update(particles, fitness_func)
+        history.append(deepcopy(particles))
+    return history
+
 
 if __name__ == "__main__":
-    initialize(10, -2, 2, -2, 2)
+    history = train(2, 10, rastrigin)
