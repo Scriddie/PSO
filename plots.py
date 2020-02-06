e87178790f7ee82601908369a18f54f2be925e5e
Alexander Reisach <alexander.reisach@gmail.com>
2020-02-06 13:14:52 +0100

    plot is better now, still diverging

--- plots.py
+++ plots.py
@@ -21,24 +21,27 @@ def plot_3d(fn, x1_low, x1_high, x2_low, x2_high, stepsize=0.1):
     plt.show()
 
 
-def visualize_heatmap(fn, history):
+def visualize_heatmap(fn, history, extent):
     # heatmap version
     buffer = []
-    for state in history:
+    for i, state in enumerate(history):
+        if i % 5 != 0:
+            continue
         plt.close("all")
         fig = plt.figure()
-        X = np.arange(-2, 2, 0.1)
-        Y = np.arange(-2, 2, 0.1)
+        X = np.arange(extent[0], extent[1], 0.1)
+        Y = np.arange(extent[2], extent[3], 0.1)
         X_grid, Y_grid = np.meshgrid(X, Y)
         Z = fn(X_grid, Y_grid)
-        sns.heatmap(Z)
+        heat = plt.imshow(Z, extent=extent, cmap='hot')
+        # heat = sns.heatmap(Z)
+        # heat.invert_yaxis()
 
         # visualize particles
         x_points = [i["pos"][0] for i in state]
         y_points = [i["pos"][0] for i in state]
         z_points = [i["fit"] for i in state]
         sns.scatterplot(x=x_points, y=y_points)
-
 
         fig.canvas.draw()       # draw the canvas, cache the renderer
         image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
--- pso.py
+++ pso.py
@@ -9,57 +9,101 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import seaborn as sns
 import imageio
+from importlib import reload
+reload(plots)
+reload(utils)
 
 best_fitness = np.inf
+best_fit_hist = [best_fitness]
 best_position = np.array([0, 0])
+best_pos_hist = [best_position]
 
-def initialize(n, x_min, x_max, y_min, y_max):
+def initialize(fn, n, x_min, x_max, y_min, y_max):
+    global best_fitness
+    global best_position
     particles = [
         {
             "pos": np.array([np.random.uniform(x_min, x_max),
                              np.random.uniform(y_min, y_max)]),
             "v": np.array([np.random.uniform(0, 0.1),
                            np.random.uniform(0, 0.1)]),
-            "fit": np.inf
         }
         for i in range(n)
     ]
+    # TODO: this doesbt seem to work
     for p in particles:
         p["best_pos"] = (p["pos"])
-    return particles
-
-def update(particles, fitness_func):
-    global best_fitness
-    global best_position
-    update = []
-    for p in particles:
-        r = np.random.uniform(0, 1)
-        v = (
-            p["v"] +
-            r * (p["pos"] - p["best_pos"]) +
-            r * (p["pos"] - best_position)
-        )
-        update.append(v)
-    for i, p in enumerate(particles):
-        p["pos"] += update[i]
-        p["fit"] = fitness_func(p["pos"][0], p["pos"][1])
-        if p["fit"] > best_fitness:
+        p["fit"] = fn(p["pos"][0], p["pos"][1])
+        p["best_fit"] = p["fit"]
+        if p["fit"] < best_fitness:
             best_fitness = p["fit"]
             best_position = p["pos"]
     return particles
 
-def train(num_particles, num_iter, fitness_func):
-    particles = initialize(num_particles, -2, 2, -2, 2)
+
+def update(fn, particles):
+    global best_fitness
+    global best_position
+    update = []
+    for p in particles:
+        r_own = np.random.uniform(0, 1)
+        r_global = np.random.uniform(0, 1)
+        prev_speed = p["v"]
+        own_best_diff = r_own * -(p["pos"] - p["best_pos"])
+        global_best_diff = r_global * -(p["pos"] - best_position)
+        v = (prev_speed + own_best_diff + global_best_diff)
+        v_total = np.sqrt(np.sum(np.square(v)))
+        # some rather arbitrary speed clipping
+        max_speed = 2
+        if v_total > max_speed:
+            v = (v/v_total) * max_speed
+        update.append(v)
+    # TODO: seems like speed is changing but direction is not?
+    for i, p in enumerate(particles):
+        p["v"] = update[i]
+        p["pos"] += update[i] * 0.01
+        p["fit"] = fn(p["pos"][0], p["pos"][1])
+        if p["fit"] < p["best_fit"]:
+            p["best_fit"] = p["fit"]
+            p["best_pos"] = p["pos"]
+        if p["fit"] < best_fitness:
+            best_fitness = p["fit"]
+            best_position = p["pos"]
+    return particles
+    
+
+def train(fn, num_particles, num_iter, extent):
+    global best_position
+    global best_pos_hist
+    particles = initialize(fn, num_particles, *extent)
     history = []
     for i in range(num_iter):
-        particles = update(particles, fitness_func)
+        particles = update(fn, particles)
         history.append(deepcopy(particles))
+        best_pos_hist.append(deepcopy(best_position))
+        best_fit_hist.append(deepcopy(best_fitness))
     return history
 
+def debug(history):
+    global best_fitness
+    global best_position
+    for i, state in enumerate(history):
+        v = [p["v"] for p in state]
+        avg_v = np.round(np.mean(v), 3)
+        avg_x = np.round(np.mean([p["pos"][0] for p in state]))
+        avg_y = np.round(np.mean([p["pos"][1] for p in state]))
+        print(f"{i}\t best_position: {np.round(best_pos_hist[i], 3)} " + 
+              f"({np.round(best_fit_hist[i])}) - avg speed: {avg_v}" +
+              f"- avg position: ({avg_x}, {avg_y})")
+
+
 if __name__ == "__main__":
-    history = train(10, 50, utils.rastrigin)
-    # plots.visualize_3D(history)
-    plots.visualize_heatmap(utils.rastrigin, history)
+    fn = utils.rosenbrock
+    extent = [-2, 2, -2, 2]
+    history = train(fn, 10, 200, extent)
+    debug(history)
+    # plots.visualize_3D(utils.rastrigin, history)
+    plots.visualize_heatmap(fn, history, extent)
 
 
 
